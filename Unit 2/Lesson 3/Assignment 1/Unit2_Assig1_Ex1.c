bba1afadbc715f1af7c1d6e60df5572272bcbb59
mazenm01 <139022817+mazenm01@users.noreply.github.com>
2023-07-19 16:11:37 +0300

    Add files via upload

---
+++ Unit 2/Lesson 3/Assignment 1/Unit2_Assig1_Ex1.c
@@ -0,0 +1,13 @@
+/*
+ * Unit2_Assig1_Ex1.c
+ *
+ *  Created on: Jul 19, 2023
+ *      Author: Mazen
+ */
+#include <stdio.h>
+
+int main()
+{
+	printf("C Programming");
+	return 0;
+}
---
+++ Unit 2/Lesson 3/Assignment 1/Unit2_Assig1_Ex2.c
@@ -0,0 +1,17 @@
+/*
+ * Unit2_Assig1_Ex2.c
+ *
+ *  Created on: Jul 19, 2023
+ *      Author: Mazen
+ */
+#include <stdio.h>
+
+int main()
+{ int c ;
+printf("Enter a integer:");
+fflush(stdout); fflush(stdin);
+scanf("%d",&c);
+printf("You entered:%d",c);
+return 0;
+}
+
